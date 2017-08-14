27330324aeb7041fe1ef6acaa9dd1fe4d7850f8c
Miliano Mikol <3PIPHANIZER@Milianos-MacBook-Pro.local>
2017-08-13 22:41:04 -0700

    first commit

--- final-project/main.py
+++ final-project/main.py
@@ -15,10 +15,6 @@ class CreateEventHandler(webapp2.RequestHandler):
     def get(self):
         create_event_template = env.get_template('create_event.html')
         self.response.write(create_event_template.render())
-
-    def post(self):
-        event_created_template = env.get_template('event_created.html')
-        self.response.write(event_created_template.render())
         event = Event(
             event_name=self.request.get('event_name'),
             event_location=self.request.get('event_location'),
@@ -30,20 +26,44 @@ class CreateEventHandler(webapp2.RequestHandler):
             event_category=self.request.get('event_category')
         )
 
+    def post(self):
+        event_created_template = env.get_template('event_created.html')
+        self.response.write(event_created_template.render())
+        template_variables= {
+            'event_name': self.request.get('event_name'),
+            'event_location': self.request.get('event_location'),
+            'event_date': datetime.date{
+                'event_year': int(self.request.get('event_year')),
+                'event_month': int(self.request.get('event_month')),
+                'event_day': int(self.request.get('event_day'))
+                },
+            'event_description':self.request.get('event_description'),
+            'event_category': self.request.get('event_category')
+        }
+        self.response.write(event_created_template.render(template_variables))
+
 class CreateUserHandler(webapp2.RequestHandler):
     def get(self):
         create_user_template = env.get_template('create_user.html')
         self.response.write(create_user_template.render())
-
-    def post(self):
-        user_created_template = env.get_template('user_created.html')
-        self.response.write(user_created_template.render())
         user = User(
             real_name=self.request.get('real_name'),
             screen_name=self.request.get('screen_name'),
             user_email=self.request.get('user_email'),
             user_password=self.request.get('user_password')
         )
+
+    def post(self):
+        user_created_template = env.get_template('user_created.html')
+        self.response.write(user_created_template.render())
+        template_variables= {
+            'real_name': self.request.get('real_name'),
+            'screen_name': self.request.get('screen_name'),
+            'user_email': self.request.get('user_email'),
+            'user_password':self.request.get('user_password')
+        }
+        self.response.write(user_created_template.render(template_variables))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
