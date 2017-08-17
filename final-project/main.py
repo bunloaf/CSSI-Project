e6e473c04229b96da4c8ec451f27d73ff1f5c292
3piphanizer <3piphanizer@gmail.com>
2017-08-16 21:18:31 -0700

    commit

--- final-project/main.py
+++ final-project/main.py
@@ -48,10 +48,7 @@ class SubmitEventHandler(webapp2.RequestHandler):
 
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
         profile_template = env.get_template('profile2.html')
-=======
->>>>>>> f843fbc60de9140e91c6bb1f08ce171e3b547530
         user = users.get_current_user()
         profile_template = env.get_template('profile.html')
         vars = {
@@ -63,8 +60,7 @@ class ProfileHandler(webapp2.RequestHandler):
             'pronouns': Profile.pronouns
         }
         self.response.write(profile_template.render())
-<<<<<<< HEAD
-        # 
+        #
         # if profile:
         #     vars = {
         #         'name': Profile.name,
@@ -75,7 +71,6 @@ class ProfileHandler(webapp2.RequestHandler):
         #         'pronouns': Profile.pronouns
         #     }
         # self.response.write(profile_template.render(vars))
-=======
 
         profile = Profile.query(Profile.user_id == user.user_id())
         if profile:
@@ -88,8 +83,6 @@ class ProfileHandler(webapp2.RequestHandler):
                 'pronouns': Profile.pronouns
             }
         self.response.write(profile_template.render(vars))
->>>>>>> f843fbc60de9140e91c6bb1f08ce171e3b547530
-
 
     def post(self):
         user_id = users.get_current_user().user_id()
